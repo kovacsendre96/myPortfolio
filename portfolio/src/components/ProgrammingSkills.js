91a3c10cffe2f8de71e98b1cf1104de5c764ac12
Kovács Endre <kovacsendre96@gmail.com>
2021-02-08 13:58:54 +0100

    slider added

--- portfolio/src/components/ProgrammingSkills.js
+++ portfolio/src/components/ProgrammingSkills.js
@@ -1,76 +1,138 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import Html from '../img/html5.png'
 import Css from '../img/css-logo.png'
 import Js from '../img/js.png';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
+const ProgrammingSkills = () => {
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    return (
+        <Container>
+
+            <Slider className='slider'{...settings}>
+
+                <Wrapper>
+
+                    <Card>
+                        <ImgWrapper>
+                            <img src={Html} alt="html logo" />
+                        </ImgWrapper>
+                        <Content>
+                            <p> <FontAwesomeIcon style={{ color: "#FFAFA0" }} icon={faCheck} /> Basic Knowledge of HTML</p>
+                            <p> <FontAwesomeIcon style={{ color: "#FFAFA0" }} icon={faCheck} /> Structure construction</p>
+                            <p> <FontAwesomeIcon style={{ color: "#FFAFA0" }} icon={faCheck} /> Froms and Validation</p>
 
 
-const ProgrammingSkills = () =>{
+                        </Content>
+                    </Card>
+                </Wrapper>
 
-    return(
+                <Wrapper>
+                    <Card>
+                        <ImgWrapper style={{ background: "#8cd7f7" }}>
+                            <img src={Css} alt="css logo" />
+                        </ImgWrapper>
+                        <Content>
+                            <p> <FontAwesomeIcon style={{ color: "#8CD7F7" }} icon={faCheck} /> Basic Knowledge of CSS</p>
+                            <p> <FontAwesomeIcon style={{ color: "#8CD7F7" }} icon={faCheck} /> Responsive desing and Media Queries</p>
+                            <p> <FontAwesomeIcon style={{ color: "#8CD7F7" }} icon={faCheck} /> Floats Positioning</p>
+                            <p> <FontAwesomeIcon style={{ color: "#8CD7F7" }} icon={faCheck} /> CSS Grid, Flex Box</p>
+                        </Content>
+                    </Card>
+                </Wrapper>
+                <Wrapper>
+                    <Card>
+                        <ImgWrapper style={{ background: "#f2f268" }}>
+                            <img src={Js} alt="javascript logo" />
+                        </ImgWrapper>
+                        <Content>
+                            <p> <FontAwesomeIcon style={{ color: "#f2f268" }} icon={faCheck} /> Syntax and Basic Construct</p>
+                            <p> <FontAwesomeIcon style={{ color: "#f2f268" }} icon={faCheck} /> DOM Manipulation with Vanilla JS and jQuery</p>
+                            <p> <FontAwesomeIcon style={{ color: "#f2f268" }} icon={faCheck} /> Basics of React.js</p>
+                            <p> <FontAwesomeIcon style={{ color: "#f2f268" }} icon={faCheck} /> CSS in JS</p>
+                        </Content>
+                    </Card>
+                </Wrapper>
 
-        <Wrapper>
-           
-
-            <Card>
-                <ImgWrapper>
-                    <img src={Html} alt="html logo" />
-                </ImgWrapper>
-                <Content>
-                    <p> <FontAwesomeIcon style={{color:"#FFAFA0"}} icon={faCheck} /> Basic Knowledge of HTML</p>                                                                           
-                    <p> <FontAwesomeIcon style={{color:"#FFAFA0"}} icon={faCheck} /> Structure construction</p>
-                    <p> <FontAwesomeIcon style={{color:"#FFAFA0"}} icon={faCheck} /> Froms and Validation</p>
-                   
-                   
-                  
-                </Content>
-            </Card>
-
-
-            <Card>
-                <ImgWrapper style={{background:"#8cd7f7"}}>
-                    <img src={Css} alt="css logo" /> 
-                </ImgWrapper>
-                <Content>
-                    <p> <FontAwesomeIcon style={{color:"#8CD7F7"}} icon={faCheck} /> Basic Knowledge of CSS</p>
-                    <p> <FontAwesomeIcon style={{color:"#8CD7F7"}} icon={faCheck} /> Responsive desing and Media Queries</p>
-                    <p> <FontAwesomeIcon style={{color:"#8CD7F7"}} icon={faCheck} /> Floats Positioning</p>
-                    <p> <FontAwesomeIcon style={{color:"#8CD7F7"}} icon={faCheck} /> CSS Grid, Flex Box</p>
-                </Content>
-            </Card>
-
-
-            <Card>
-                <ImgWrapper style={{background:"#f2f268"}}>
-                    <img src={Js} alt="javascript logo" /> 
-                </ImgWrapper>
-                <Content>
-                    <p> <FontAwesomeIcon style={{color:"#f2f268"}} icon={faCheck} /> Syntax and Basic Construct</p>
-                    <p> <FontAwesomeIcon style={{color:"#f2f268"}} icon={faCheck} /> DOM Manipulation with Vanilla JS and jQuery</p>
-                    <p> <FontAwesomeIcon style={{color:"#f2f268"}} icon={faCheck} /> Basics of React.js</p>
-                    <p> <FontAwesomeIcon style={{color:"#f2f268"}} icon={faCheck} /> CSS in JS</p>
-                </Content>
-            </Card>
-
-          
-
-        </Wrapper>
-
+            </Slider>
+        </Container>
 
     );
 
 
 };
 
+
+
+
+
 export default ProgrammingSkills;
+
+const Container = styled.div`
+         .slider {
+      width:70%;
+      margin:0 auto 0 auto;
+      position: relative;
+      
+
+
+.slick-prev:before,
+.slick-next:before {
+  color: #329555;
+}
+    }
+`;
 
 
 const Wrapper = styled.div`
     display:flex;
-    margin: 0 auto 0 auto ;
+    margin: 10px auto 0 auto ;
     width:80%;
+  
+ 
+
+   
  
 
 
@@ -98,10 +160,21 @@ const Card = styled.div`
    transition:0.5s;
     &:hover{
    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-   transform:scale(1.1);
+   transform:scale(1.05);
    }
+   @media screen and (max-width: 1300px) {
+    width:270px;
+    height:370px;
+  }
 
-
+   @media screen and (max-width: 900px) {
+    width:250px;
+    height:350px;
+  }
+   @media screen and (max-width: 600px) {
+    width:220px;
+    height:320px;
+  }
 `;
 
 const ImgWrapper = styled.div`
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -6,65 +6,98 @@ import wedding from '../img/wedding.png';
 import blog from '../img/blog.png';
 import music from '../img/music.jpg';
 import todo from "../img/todo.png";
-import {Line} from "./styles";
+import { Line } from "./styles";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 
 
-const Projects = () =>{
-
-return(
-    <ProjectsStyle id="projects">
-
-        <h1>My projects</h1>
-        <Line />
-        <Container>
-
-      
-            <Card style={{background:`url(${music})`, backgroundSize:"cover",filter:""}}>
-            <a href="https://kovacsendre96.github.io/music-player/" target="blanc" ></a> 
-            <CardBottom> <h5>Music player</h5> <a href="https://github.com/kovacsendre96/react-todo" target="_blanc"><FontAwesomeIcon size="2x" icon={faGithub}/>Link</a></CardBottom>
-            </Card>
-
-       
-
-            <Card style={{background:`url(${wedding})`, backgroundSize:"cover"}}>
-            <a href="https://kovacsendre96.github.io/my-components/wedding.html" target="blanc2" ></a> 
-            <CardBottom> <h5>Wedding page</h5> <a href="https://github.com/kovacsendre96/my-components" target="_blanc2"><FontAwesomeIcon size="2x" icon={faGithub}/>Link</a></CardBottom>
-
-            </Card>
+const Projects = () => {
 
 
-            
-            <Card style={{background:`url(${blog})`, backgroundSize:"cover"}}>
-            <a href="https://sadap93.github.io/blog/" target="blanc3" ></a> 
-            <CardBottom> <h5>Blog</h5> <a href="https://github.com/kovacsendre96/blog" target="_blanc3"><FontAwesomeIcon size="2x" icon={faGithub}/>Link</a></CardBottom>
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
 
-            </Card>
-
-
-            
-            <Card style={{background:`url(${todo})`, backgroundSize:"cover"}}>
-            <a href="https://kovacsendre96.github.io/react-todo/" target="blanc4" ></a>
-            <CardBottom> <h5>To do app</h5> <a href="https://github.com/kovacsendre96/react-todo" target="_blanc4"><FontAwesomeIcon size="2x" icon={faGithub}/>Link</a></CardBottom>
-
-            </Card>
-        
+        responsive: [
 
 
-            <Card style={{background:`url(${clock})`, backgroundSize:"cover"}}>
-            <a href="https://kovacsendre96.github.io/clock/" target="blanc6" ></a>
-            <CardBottom> <h5>Clock</h5> <a href="https://github.com/kovacsendre96/clock" target="_blanc5"><FontAwesomeIcon size="2x" icon={faGithub}/>Link</a></CardBottom>
-            </Card>
-      
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    return (
 
 
-        </Container>
-    
-    </ProjectsStyle>
 
-);
+        <ProjectsStyle id="projects">
+            <h1>My projects</h1>
+            <Line />
+
+            <Slider className='slider'{...settings}>
+                <Container>
+
+                    <Card style={{ background: `url(${music})`, backgroundSize: "cover", filter: "" }}>
+                        <a href="https://kovacsendre96.github.io/music-player/" target="blanc" ></a>
+                        <CardBottom> <h5>Music player</h5> <a href="https://github.com/kovacsendre96/react-todo" target="_blanc"><FontAwesomeIcon size="2x" icon={faGithub} />Link</a></CardBottom>
+                    </Card>
+
+                </Container>
+                <Container>
+
+                    <Card style={{ background: `url(${wedding})`, backgroundSize: "cover" }}>
+                        <a href="https://kovacsendre96.github.io/my-components/wedding.html" target="blanc2" ></a>
+                        <CardBottom> <h5>Wedding page</h5> <a href="https://github.com/kovacsendre96/my-components" target="_blanc2"><FontAwesomeIcon size="2x" icon={faGithub} />Link</a></CardBottom>
+                    </Card>
+                </Container>
+
+                <Container>
+                    <Card style={{ background: `url(${blog})`, backgroundSize: "cover" }}>
+                        <a href="https://sadap93.github.io/blog/" target="blanc3" ></a>
+                        <CardBottom> <h5>Blog</h5> <a href="https://github.com/kovacsendre96/blog" target="_blanc3"><FontAwesomeIcon size="2x" icon={faGithub} />Link</a></CardBottom>
+                    </Card>
+                </Container>
+
+                <Container>
+                    <Card style={{ background: `url(${todo})`, backgroundSize: "cover" }}>
+                        <a href="https://kovacsendre96.github.io/react-todo/" target="blanc4" ></a>
+                        <CardBottom> <h5>To do app</h5> <a href="https://github.com/kovacsendre96/react-todo" target="_blanc4"><FontAwesomeIcon size="2x" icon={faGithub} />Link</a></CardBottom>
+                    </Card>
+                </Container>
+
+                <Container>
+                    <Card style={{ background: `url(${clock})`, backgroundSize: "cover" }}>
+                        <a href="https://kovacsendre96.github.io/clock/" target="blanc6" ></a>
+                        <CardBottom> <h5>Clock</h5> <a href="https://github.com/kovacsendre96/clock" target="_blanc5"><FontAwesomeIcon size="2x" icon={faGithub} />Link</a></CardBottom>
+                    </Card>
+
+                </Container>
+            </Slider>
+        </ProjectsStyle>
+
+    );
 
 };
 
@@ -72,7 +105,11 @@ return(
 export default Projects;
 
 
-const ProjectsStyle =styled.div`
+
+
+
+
+const ProjectsStyle = styled.div`
 background:url(${blackWallpaper});
 background-size:cover;
 display:flex;
@@ -84,7 +121,17 @@ padding-bottom:100px;
             padding-top:50px;
   }
     
+    .slider {
+      width:70%;
+      margin:0 auto 0 auto;
+      position: relative;
 
+      .slick-prev:before,
+.slick-next:before {
+  color: #329555;
+}
+    }
+    
 `;
 
 const Container = styled.div`
@@ -92,8 +139,8 @@ display:flex;
 width:70%;
 margin: 0 auto 0 auto;
 justify-content:center;
-flex-wrap:wrap;
 align-items:center;
+
 `;
 
 const Card = styled.div`
@@ -111,13 +158,15 @@ const Card = styled.div`
    height:220px;
    display:flex;
    flex-direction:column;
-   justify-content:space-around;
    align-items:center;
    border-radius: 5px 25px 5px 25px;
    margin-bottom:10px;
-   margin:15px 15px 15px 15px;
+   margin:15px auto 15px auto;
    transition:0.8s;
- 
+   @media screen and (max-width: 600px) {
+    width:280px;
+   height:190px;  
+  }
   
    &:hover{
     transform:scale(1.05);
@@ -167,6 +216,3 @@ const CardBottom = styled.div`
     
    
 `;
-
-
-  
